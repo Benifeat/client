fec2e4829c34abfc821404b2a8ec5866acb2b5a6
aori41 <aori444@gmail.com>
2024-07-10 22:23:10 +0300

    [FIT-5] Added global colors from the list in figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalColors from './components/atoms/GlobalColors/colors';
 import './App.css';
 
 function App() {
 
   return <>
     <Router>
+      <GlobalColors />
       <Routes>
         <Route path="/" />
       </Routes>
---
+++ src/components/atoms/GlobalColors/colors.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalColors = createGlobalStyle`
+	:root {
+		--primary: '#AFEA0D';
+		--black: '#202117';
+		--black-body-text: '#676A4E';
+		--grey: '#9A9C95';
+		--line: '#BDC5A7';
+		--background: '#F7F7F7';
+		--white: '#FFFFFF';
+		--red: '#EA0D0D';
+		--pink: '#F75F7B';
+		--orange: '#FFA500';
+		--purple: '#7C0DEA';
+		--blue: '#4B45ED';
+		--cyan: '#45BBED';
+	}
+		
+	body {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}`;
+
+export default GlobalColors;
