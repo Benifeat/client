05870f3d763eff46669707f817cf7d846d050e19
aori41 <aori444@gmail.com>
2024-07-16 15:04:23 +0300

    [FIT-5] Fixed changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GlobalColors from './components/atoms/GlobalColors/colors';
 import './App.css';
 
 function App() {
 
-  return <>
-    <Router>
-      <GlobalColors />
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </Router>
-  </>
 }
 
 export default App;
