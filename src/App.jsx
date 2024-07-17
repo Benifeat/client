cb955d0335e4f0876f3e5a9aae8056c6e9c6abd7
Alina Novofastovskaia <alinanovof@gmail.com>
2024-07-17 12:47:11 +0300

    [FIT-5] fixed conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
