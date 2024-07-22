3d43fd2909232f8e732e228769d341009a09abf8
xMagician12 <magician2245@gmail.com>
2024-07-22 16:14:02 +0300

    [FIT-15] Added describe block for testing

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -5,47 +5,49 @@ import { colors } from '../colors';
 
 afterEach(cleanup);
 
-test('Renders the button with the correct label', () => {
-	render(<Button onClick={() => { }}>Click Me</Button>);
+describe('Button Component', () => {
+	test('Renders the button with the correct label', () => {
+		render(<Button onClick={() => { }}>Click Me</Button>);
 
-	const buttonElement = screen.getByText(/Click Me/i);
+		const buttonElement = screen.getByText(/Click Me/i);
 
-	expect(buttonElement).toBeInTheDocument();
-});
+		expect(buttonElement).toBeInTheDocument();
+	});
 
-test('Check sending button type', () => {
-	render(<Button onClick={() => { }} type='premium'>Click Me</Button>);
+	test('Check sending button type', () => {
+		render(<Button onClick={() => { }} type='premium'>Click Me</Button>);
 
-	const buttonElement = screen.getByText(/Click Me/i);
+		const buttonElement = screen.getByText(/Click Me/i);
 
-	expect(buttonElement).toHaveStyle(`background-color: ${colors.orange}`);
-});
+		expect(buttonElement).toHaveStyle(`background-color: ${colors.orange}`);
+	});
 
-test('Check default button if not sending props', () => {
-	render(<Button onClick={() => { }}>Click Me</Button>);
+	test('Check default button if not sending props', () => {
+		render(<Button onClick={() => { }}>Click Me</Button>);
 
-	const buttonElement = screen.getByText(/Click Me/i);
+		const buttonElement = screen.getByText(/Click Me/i);
 
-	expect(buttonElement).toHaveStyle(`background-color: ${colors.primary}`);
-	expect(buttonElement).toHaveStyle(`width: 351px`);
-});
+		expect(buttonElement).toHaveStyle(`background-color: ${colors.primary}`);
+		expect(buttonElement).toHaveStyle(`width: 351px`);
+	});
 
-test('Check custom styles for specific type', () => {
-	render(<Button onClick={() => { }} type='outlined' backgroundColor={colors.cyan} color={colors.orange}>Click Me</Button>);
+	test('Check custom styles for specific type', () => {
+		render(<Button onClick={() => { }} type='outlined' backgroundColor={colors.cyan} color={colors.orange}>Click Me</Button>);
 
-	const buttonElement = screen.getByText(/Click Me/i);
+		const buttonElement = screen.getByText(/Click Me/i);
 
-	expect(buttonElement).toHaveStyle(`background-color: ${colors.cyan}`);
-	expect(buttonElement).toHaveStyle(`color: ${colors.orange}`);
-});
+		expect(buttonElement).toHaveStyle(`background-color: ${colors.cyan}`);
+		expect(buttonElement).toHaveStyle(`color: ${colors.orange}`);
+	});
 
-test('calls onClick prop when clicked', () => {
-	const handleClick = jest.fn();
+	test('calls onClick prop when clicked', () => {
+		const handleClick = jest.fn();
 
-	render(<Button onClick={handleClick}>Click Me</Button>);
+		render(<Button onClick={handleClick}>Click Me</Button>);
 
-	const buttonElement = screen.getByText(/Click Me/i);
+		const buttonElement = screen.getByText(/Click Me/i);
 
-	fireEvent.click(buttonElement);
-	expect(handleClick).toHaveBeenCalled();
+		fireEvent.click(buttonElement);
+		expect(handleClick).toHaveBeenCalled();
+	});
 });
